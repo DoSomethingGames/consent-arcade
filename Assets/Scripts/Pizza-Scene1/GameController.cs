da3f687002ed347d46be49005ff1eba8d64d3786
jon uy <jonxuy@gmail.com>
2015-07-28 23:41:08 -0400

    Start of initial scene for player selecting their own ingredients

---
+++ Assets/Scripts/Pizza-Scene1/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private int numSelected = 0;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("GAME CONTROLLER STARTED");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/**
+	 * Function called when an ingredient is clicked.
+	 * 
+	 * @param Object ingredient
+	 *   The Ingredient selected on click
+	 */
+	public void OnIngredientClick (GameObject ingredient) {
+		Debug.Log("clicked");
+		numSelected++;
+
+		Ingredient selected = ingredient.GetComponent<Ingredient>();
+
+		Debug.Log("Ingredient clicked. type: " + selected.type + ", num selected: " + numSelected);
+	}
+}
---
+++ Assets/Scripts/Pizza-Scene1/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ingredient : MonoBehaviour {
+
+	public enum INGREDIENT_TYPE {
+		ANCHOVIES,
+		BACON,
+		EXTRA_CHEESE,
+		MUSHROOM,
+		PEPPER,
+		PEPPERONI,
+		PINEAPPLE,
+		SAUSAGE
+	};
+
+	public INGREDIENT_TYPE type;
+
+}
